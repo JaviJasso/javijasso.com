a95c1b246190ba870ef6f3986f451130b421836c
Javi <jjasso1@asu.edu>
2017-09-20 15:11:40 -0400

    icons

--- src/components/Achivements.js
+++ src/components/Achivements.js
@@ -15,14 +15,29 @@ class Achivements extends Component {
             </h1>
           </div>
           <div className='skillsBody'>
-            <h2>OPERATIONG SYSTEMS</h2>
+            <div className='icon'>
+              <i className="fa fa-desktop" aria-hidden=""></i>
+            </div>
+            <h2>OPERATING SYSTEMS</h2>
             <p>Microsoft Windows 10, Mac OS X, Apple iOS, Google Android.</p>
+            <div className='icon'>
+              <i className="fa fa-building" aria-hidden=""></i>
+            </div>
             <h2>DEVOPS</h2>
             <p>Git, GitHub, MySQL</p>
+            <div className='icon'>
+              <i className="fa fa-language" aria-hidden=""></i>
+            </div>
             <h2>LANGUAGES</h2>
             <p>React, SQL, HTML, JavaScript, XML.</p>
+            <div className='icon'>
+              <i className="fa fa-wrench" aria-hidden=""></i>
+            </div>
             <h2>WEB TOOLS</h2>
             <p>JSON, JQUERY</p>
+            <div className='icon'>
+              <i className="fa fa-user" aria-hidden=""></i>
+            </div>
             <h2>UX & UI</h2>
             <p>HTML/CSS, JavaScript, Slack</p>
           </div>
